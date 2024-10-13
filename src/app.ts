367c049cfc7a6deef5e88e3cacec21567c0cec19
badAl <badalsingh120403@gmail.com>
2024-10-14 04:12:43 +0530

    successfully connected the mysql db

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,9 @@
 import express, { Request, Response } from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
 
 const app = express();
 
@@ -22,21 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(logger("tiny"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 app.use(limiter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
 
-app.use((err: any, req: Request, res: Response, next: any) => {
-  console.error(err.stack);
-  if (err.status) {
-    res.status(err.status).json({ message: err.message });
-  } else {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
-});
+export default app;

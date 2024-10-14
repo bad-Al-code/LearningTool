5e32960311f1a4e0e1c8c373f4221bc2c5aae7e2
badAl <badalsingh120403@gmail.com>
2024-10-14 21:02:01 +0530

    protected route: chack if token is valid or not that is prensent in cookie and then validate the cookie

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -38,7 +38,12 @@ export const registerUser = async (
     };
 
     const user = await createUser(newUser);
-    res.status(201).json({ message: "User created successfully", user });
+    res
+      .status(201)
+      .json({
+        message: "User created successfully",
+        user: { id: user?.id, email: user?.email },
+      });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error creating user," });
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,18 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export const authToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.cookies.token;
 
   if (!token) {
     res.status(401).json({ message: "Acess denied" });
+    return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_TOKEN as string);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
     (req as any).user = decoded; // req object
     next();
   } catch (error) {
     res.status(403).json({ message: "Invalid token" });
+    return;
   }
 };

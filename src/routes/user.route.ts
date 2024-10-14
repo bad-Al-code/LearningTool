5bcf430f906a9d4c24b347b384c36c48a0dec2a7
badAl <badalsingh120403@gmail.com>
2024-10-14 21:02:01 +0530

    protected route: chack if token is valid or not that is prensent in cookie and then validate the cookie

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
 import { loginUser, registerUser } from "../controllers/user.controller";
+import { authenticateToken } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.get("/profile", authenticateToken, (req, res) => {
+  const user = (req as any).user;
+  res.status(200).json({ message: "Profile data", user });
+});
 
 export default router;

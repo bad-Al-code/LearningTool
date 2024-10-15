09cb94dc6b18b39c5ed1565271d214c07a6ca386
badAl <badalsingh120403@gmail.com>
2024-10-15 15:25:39 +0530

    add a route to verify the OTP before allowing the user to reset their password

--- backend/auth/src/validation/user.validation.ts
+++ backend/auth/src/validation/user.validation.ts
@@ -38,6 +38,27 @@ export const userLoginValidation = z.object({
     }),
 });
 
+export const userResetPasswordSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters long")
+      .max(50, "Password should be less than 50 characters")
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase character",
+      })
+      .regex(/[0-9]/, { message: "Password must contain at least one number" })
+      .regex(/[\W_]/, {
+        message: "Password must contain at least one special character",
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
+
 export type UserRegisterInput = z.infer<typeof userRegisterSchema>;
 export type UserVerifyOTPInput = z.infer<typeof userVerifyOTPSchema>;
 export type UserLoginValidation = z.infer<typeof userLoginValidation>;
+export type UserResetPasswordSchema = z.infer<typeof userResetPasswordSchema>;

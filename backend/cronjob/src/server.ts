d577ba8768473a19176fdd2729262d60e083709f
badAl <badalsingh120403@gmail.com>
2024-10-18 23:45:40 +0530

    Implement multiple job scheduling with time zone support (IST)

--- backend/cronjob/src/server.ts
+++ backend/cronjob/src/server.ts
@@ -2,6 +2,14 @@ function task() {
     console.log(`Task execued at ${new Date().toLocaleTimeString()}`);
 }
 
+function job2() {
+    console.log(`Job 2 executed at ${new Date().toLocaleDateString()}`);
+}
+
+function job1() {
+    console.log(`Job 1 executed at ${new Date().toLocaleDateString()}`);
+}
+
 function getNextExecutionDelay(
     hour: number,
     minute: number,
@@ -19,14 +27,91 @@ function getNextExecutionDelay(
     return nextExecution.getTime() - now.getTime();
 }
 
-function scheduleDailyTask(hour: number, minute: number, second: number = 0) {
+function scheduleJob(
+    job: () => void,
+    hour: number,
+    minute: number,
+    second: number = 0
+) {
     const delay = getNextExecutionDelay(hour, minute, second);
 
     setTimeout(() => {
-        task();
+        job();
 
-        setInterval(task, 24 * 60 * 60 * 1000);
+        setInterval(job, 24 * 60 * 60 * 1000);
     }, delay);
 }
 
-scheduleDailyTask(23, 16);
+function getNextExecutionInTimeZone(
+    hour: number,
+    minute: number,
+    timeZone: string
+) {
+    const now = new Date();
+    const nextExecution = new Date(
+        new Intl.DateTimeFormat("en-us", {
+            timeZone: timeZone,
+            year: "numeric",
+            month: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        }).format(now)
+    );
+
+    nextExecution.setHours(hour, minute, 0, 0);
+
+    if (nextExecution <= now) {
+        nextExecution.setDate(nextExecution.getDate() + 1);
+    }
+
+    return nextExecution.getTime() - now.getTime();
+}
+
+function scheduleJonInTimeZone(
+    job: () => void,
+    hour: number,
+    minute: number,
+    timeZone: string
+) {
+    const delay = getNextExecutionInTimeZone(hour, minute, timeZone);
+
+    setTimeout(() => {
+        job();
+
+        setInterval(job, 24 * 60 * 60 * 1000);
+    }, delay);
+}
+
+function adjustForDST(
+    job: () => void,
+    hour: number,
+    minute: number,
+    timeZone: string
+) {
+    const now = new Date();
+    let lastOffset = now.getTimezoneOffset();
+
+    setInterval(() => {
+        const currentOffset = new Date().getTimezoneOffset();
+
+        if (currentOffset !== lastOffset) {
+            console.log("DST change detected. Adjusting schedule...");
+            lastOffset = currentOffset;
+
+            scheduleJob;
+        }
+    });
+}
+
+scheduleJob(job1, 23, 34, 0);
+scheduleJob(job2, 23, 34, 30);
+
+scheduleJonInTimeZone(
+    () => {
+        console.log("job in IST executed");
+    },
+    23,
+    44,
+    "Asia/Kolkata"
+);

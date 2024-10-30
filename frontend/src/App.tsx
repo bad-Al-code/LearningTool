a24c116d17097dfe5fb4b06fd8def1b0649fbfad
badAl <badalsingh120403@gmail.com>
2024-10-30 20:07:20 +0530

    staggerAnimation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const App: React.FC = () => {
+    const [showImage, setShowImage] = useState(false);
+    const handleClickImage = () => setShowImage((prev) => !prev);
+
     const boxVariants = {
         initial: {
             scale: 1,
@@ -20,50 +24,45 @@ const App: React.FC = () => {
         },
     };
 
+    const parentVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { staggerChildren: 0.8 } },
+    };
+
+    const childVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
+    };
+
     return (
-        <div className="flex justify-center items-center h-screen  ">
-            {/* {[...Array(3)].map((_, index) => (
+        <motion.div
+            className="flex justify-center items-center h-screen  "
+            variants={parentVariants}
+            initial="hidden"
+            animate="visible"
+        >
+            {[...Array(5)].map((_, index) => (
                 <motion.div
+                    className="box mt-[2rem]"
                     key={index}
-                    className="h-4 w-4 bg-red-500 rounded-full"
-                    variants={{
-                        hidden: { opacity: 0, scale: 0.8 },
-                        visible: {
-                            opacity: 1,
-                            scale: 1,
-                        },
-                        exit: {
-                            opacity: 0,
-                            scale: 0.5,
-                        },
-                    }}
-                    initial="visible"
-                    animate={{
-                        x:
-                            index === 0
-                                ? [0, 15, 0]
-                                : index === 1
-                                  ? [0, 15, 0]
-                                  : [0, 15, 0],
-                    }}
-                    transition={{
-                        duration: 1.5,
-                        ease: 'easeInOut',
-                        repeat: Infinity,
-                        times: [0, 0.5, 1],
-                        delay: index * 0.3,
-                        repeatDelay: 0.4,
-                    }}
-                />
-            ))} */}
-            <motion.div
+                    variants={childVariants}
+                ></motion.div>
+            ))}
+            {/* <motion.div
                 className="w-40 h-40 bg-blue-500 rounded-lg"
                 variants={boxVariants}
                 initial="initial"
                 whileHover="hover"
                 whileTap="click"
-            ></motion.div>
-        </div>
+            ></motion.div> */}
+
+            <motion.button
+                className="ml-10 px-4 py-2 bg-orange-500 rounded-md shadow-md hover:bg-orange-600 outline-none hover:shadow-orange-800"
+                onClick={handleClickImage}
+            >
+                {showImage ? 'Hide Images' : 'Show Images'}
+            </motion.button>
+        </motion.div>
     );
 };
 
